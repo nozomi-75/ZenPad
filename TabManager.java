1853892faf2926e97a378059a26daeecd8ad33c1
Keiaa <confinedrose@duck.com>
2025-05-11 01:58:46 +0800

    Added openNewTab which connects FOP object to EditorTab object.

--- TabManager.java
+++ TabManager.java
@@ -8,4 +8,20 @@ public class TabManager {
         // Initialize the tabbed pane
         this.tabbedPane = tabbedPane;
     }
+
+    /* The FOP object calls this method to handle file opening logic
+     * This will take a full file path and a button name
+     * The file path is passed to the EditorTab object
+     * The button name is passed to the EditorTab object to serve as the tab title
+     * The tab is added to the tabbed pane
+     * The tab is set to be selected
+    */
+
+    public void openNewTab(String filePath, String button) {
+        EditorTab newTab = new EditorTab(filePath, button);
+        tabbedPane.addTab(button, newTab.getPanel());
+
+        // Set the tab to be selected
+        tabbedPane.setSelectedIndex(tabbedPane.getTabCount() - 1);
+    }
 }
