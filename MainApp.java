d5fe11e90129a7755be3aa9b45c598b9c4b3db66
Keiaa <confinedrose@duck.com>
2025-05-11 21:02:00 +0800

    Added Javadoc support for MainApp.

--- MainApp.java
+++ MainApp.java
@@ -1,3 +1,11 @@
+/**
+ * This is the main entry point for the application.
+ * It initializes the GUI and sets up the main application frame.
+ * The GUI is created on the Event Dispatch Thread (EDT) to ensure thread safety.
+ * The main application frame is created using the AppFrame class.
+ * @see AppFrame
+ */
+
 import javax.swing.SwingUtilities;
 
 public class MainApp {
