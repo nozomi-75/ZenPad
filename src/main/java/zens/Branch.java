ea26e5b88f4f58405780511a4f5adfe98b239865
Keiaa <confinedrose@duck.com>
2025-05-18 13:01:25 +0800

    Added well-needed Javadoc for the Branch class.

--- src/main/java/zens/Branch.java
+++ src/main/java/zens/Branch.java
@@ -1,5 +1,14 @@
 package zens;
 
+/**
+ * Branch class represents a branch in the file tree structure.
+ * It contains the parent name, display names, and file names associated with the branch.
+ * 
+ * @see FileOpenerPanel
+ * @param parentName The name of the parent branch.
+ * @param displayNames An array of display names for the files in the branch.
+ * @param fileNames An array of file names for the files in the branch.
+ */
 public class Branch {
         String parentName;
         String[] displayNames;
